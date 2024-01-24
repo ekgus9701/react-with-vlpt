08d8bb1a3db666a0f2c646c32cee8b16e5033304
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-01-24 18:40:42 +0900

    feat: 1장-11

---
+++ src/UserList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+    </div>
+  );
+}
+
+function UserList() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  //map 함수는 배열 안에 있는 원소를 변환해서 새로운 배열을 만들어준다.
+  //각 고유 원소에 key 가 있어야만 배열이 업데이트 될 때 효율적으로 렌더링된다.
+
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
+}
+
+export default UserList;
